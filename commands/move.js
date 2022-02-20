a3dc680e58132340b00cfeee9f584464d579aa50
Lxwi1337 <99254477+Lxwi1337@users.noreply.github.com>
2022-02-20 21:13:46 +0100

    Update move.js

--- commands/move.js
+++ commands/move.js
@@ -3,7 +3,7 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
   name: "move",
-  description: "Moves a track to a specified position.",
+  description: "Déplace une piste vers une position spécifiée.",
   usage: "",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
@@ -22,22 +22,22 @@ module.exports = {
     if (!player)
       return client.sendTime(
         message.channel,
-        "❌ | **Nothing is playing right now...**"
+        "❌ | **Rien ne joue pour le moment...**"
       );
     if (!args[0] || !args[1])
-      return client.sendTime(message.channel, "❌ | **Invalid arguments.**");
+      return client.sendTime(message.channel, "❌ | **Arguments non valides.**");
 
     // Check if (args[0] - 1) is a valid index
     let trackNum = parseInt(args[0] - 1);
     if (trackNum < 1 || trackNum > player.queue.length - 1) {
-      return client.sendTime(message.channel, "❌ | **Invalid track number.**");
+      return client.sendTime(message.channel, "❌ | **Numéro de piste invalide.**");
     }
 
     let dest = parseInt(args[1] - 1);
     if (dest < 1 || dest > player.queue.length - 1) {
       return client.sendTime(
         message.channel,
-        "❌ | **Invalid track destination.**"
+        "❌ | **Destination de piste invalide.**"
       );
     }
 
@@ -49,7 +49,7 @@ module.exports = {
       message.channel,
       "✅ | **" +
         track.title +
-        "** has been moved to position " +
+        "** a été déplacé à la position " +
         (dest + 1) +
         "."
     );
@@ -62,14 +62,14 @@ module.exports = {
         value: "track",
         type: 4,
         required: true,
-        description: "Track to move.",
+        description: "Suivre pour se déplacer.",
       },
       {
         name: "position",
         value: "track2",
         type: 4,
         required: true,
-        description: "Moves selected track to the specified position.",
+        description: "Déplace la piste sélectionnée à la position spécifiée.",
       },
     ],
     /**
@@ -87,22 +87,22 @@ module.exports = {
       if (!player)
         return client.sendTime(
           interaction,
-          "❌ | **Nothing is playing right now...**"
+          "❌ | **Rien ne joue pour le moment...**"
         );
       if (!args[0].value || !args[1].value)
-        return client.sendTime(interaction, "❌ | **Invalid track number.**");
+        return client.sendTime(interaction, "❌ | **Numéro de piste invalide.**");
 
       // Check if (args[0] - 1) is a valid index
       let trackNum = parseInt(args[0].value - 1);
       if (trackNum < 1 || trackNum > player.queue.length - 1) {
-        return client.sendTime(interaction, "❌ | **Invalid track number.**");
+        return client.sendTime(interaction, "❌ | **Numéro de piste invalide.**");
       }
 
       let dest = parseInt(args[1].value - 1);
       if (dest < 1 || dest > player.queue.length - 1) {
         return client.sendTime(
           interaction,
-          "❌ | **Invalid track destination.**"
+          "❌ | **Destination de piste invalide.**"
         );
       }
 
@@ -114,7 +114,7 @@ module.exports = {
         interaction,
         "✅ | **" +
           track.title +
-          "** has been moved to position " +
+          "** a été déplacé à la position " +
           (dest + 1) +
           "."
       );
